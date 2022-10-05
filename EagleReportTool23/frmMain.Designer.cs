f0b3bf31060e6f9336b120c9efaadc177877f959
JEFF <JEFF@MIS-D-JEFF>
2022-10-05 14:50:21 +0900

    Add project files.

---
+++ EagleReportTool23/frmMain.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace EagleReportTool23
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem(new string[] {
+            "",
+            "",
+            "",
+            ""}, -1);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.processToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblVersion = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblPublished = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblCols = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblRows = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.butProcess = new System.Windows.Forms.ToolStripButton();
+            this.butClear = new System.Windows.Forms.ToolStripButton();
+            this.butRefresh = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.butExit = new System.Windows.Forms.ToolStripButton();
+            this.lsvFiles = new System.Windows.Forms.ListView();
+            this.colFileName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colDateModified = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1030, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.processToolStripMenuItem,
+            this.clearToolStripMenuItem,
+            this.refreshToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // processToolStripMenuItem
+            // 
+            this.processToolStripMenuItem.Name = "processToolStripMenuItem";
+            this.processToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.processToolStripMenuItem.Text = "Process";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(111, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblVersion,
+            this.lblPublished,
+            this.lblCols,
+            this.lblRows,
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 509);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1030, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(48, 17);
+            this.lblVersion.Text = "Version:";
+            // 
+            // lblPublished
+            // 
+            this.lblPublished.Name = "lblPublished";
+            this.lblPublished.Size = new System.Drawing.Size(62, 17);
+            this.lblPublished.Text = "Published:";
+            // 
+            // lblCols
+            // 
+            this.lblCols.Name = "lblCols";
+            this.lblCols.Size = new System.Drawing.Size(33, 17);
+            this.lblCols.Text = "Cols:";
+            // 
+            // lblRows
+            // 
+            this.lblRows.Name = "lblRows";
+            this.lblRows.Size = new System.Drawing.Size(38, 17);
+            this.lblRows.Text = "Rows:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(42, 17);
+            this.lblStatus.Text = "Status:";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.butProcess,
+            this.butClear,
+            this.butRefresh,
+            this.toolStripSeparator2,
+            this.butExit});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1030, 54);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // butProcess
+            // 
+            this.butProcess.Image = ((System.Drawing.Image)(resources.GetObject("butProcess.Image")));
+            this.butProcess.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.butProcess.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.butProcess.Name = "butProcess";
+            this.butProcess.Size = new System.Drawing.Size(51, 51);
+            this.butProcess.Text = "Process";
+            this.butProcess.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // butClear
+            // 
+            this.butClear.Image = ((System.Drawing.Image)(resources.GetObject("butClear.Image")));
+            this.butClear.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.butClear.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.butClear.Name = "butClear";
+            this.butClear.Size = new System.Drawing.Size(38, 51);
+            this.butClear.Text = "Clear";
+            this.butClear.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // butRefresh
+            // 
+            this.butRefresh.Image = ((System.Drawing.Image)(resources.GetObject("butRefresh.Image")));
+            this.butRefresh.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.butRefresh.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.butRefresh.Name = "butRefresh";
+            this.butRefresh.Size = new System.Drawing.Size(50, 51);
+            this.butRefresh.Text = "Refresh";
+            this.butRefresh.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 54);
+            // 
+            // butExit
+            // 
+            this.butExit.Image = ((System.Drawing.Image)(resources.GetObject("butExit.Image")));
+            this.butExit.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.butExit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.butExit.Name = "butExit";
+            this.butExit.Size = new System.Drawing.Size(36, 51);
+            this.butExit.Text = "Exit";
+            this.butExit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.butExit.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // lsvFiles
+            // 
+            this.lsvFiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lsvFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colFileName,
+            this.colDateModified});
+            this.lsvFiles.FullRowSelect = true;
+            this.lsvFiles.HideSelection = false;
+            this.lsvFiles.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem6});
+            this.lsvFiles.Location = new System.Drawing.Point(12, 81);
+            this.lsvFiles.Name = "lsvFiles";
+            this.lsvFiles.Size = new System.Drawing.Size(256, 425);
+            this.lsvFiles.TabIndex = 4;
+            this.lsvFiles.UseCompatibleStateImageBehavior = false;
+            this.lsvFiles.View = System.Windows.Forms.View.Details;
+            // 
+            // colFileName
+            // 
+            this.colFileName.Text = "Name";
+            this.colFileName.Width = 144;
+            // 
+            // colDateModified
+            // 
+            this.colDateModified.Text = "Date Modified";
+            this.colDateModified.Width = 108;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1030, 531);
+            this.Controls.Add(this.lsvFiles);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.Text = "ERT23 - Surangel and Sons, Co";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem processToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblVersion;
+        private System.Windows.Forms.ToolStripStatusLabel lblPublished;
+        private System.Windows.Forms.ToolStripStatusLabel lblCols;
+        private System.Windows.Forms.ToolStripStatusLabel lblRows;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton butProcess;
+        private System.Windows.Forms.ToolStripButton butClear;
+        private System.Windows.Forms.ToolStripButton butExit;
+        private System.Windows.Forms.ToolStripButton butRefresh;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ListView lsvFiles;
+        private System.Windows.Forms.ColumnHeader colFileName;
+        private System.Windows.Forms.ColumnHeader colDateModified;
+    }
+}
+
---
+++ EagleReportTool23/frmMain.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace EagleReportTool23
+{
+
+    public partial class frmMain : Form
+    {
+        //Declare Global Variable//
+        public const string MessageBoxCaption = "ERT23";
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        public void CloseApplication()
+        {
+            const string message = "Are you sure you want to close the application?";
+            var response = MessageBox.Show(message, MessageBoxCaption, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (response == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CloseApplication();
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            CloseApplication();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
